cf4be17f2a6b6193f6fe9b5bee848cc69e3df540
JeremyN05 <narvaezlobatojeremy@gmail.com.com>
2025-11-19 12:02:18 +0100

    Ejercici 2 V2

--- ConexionBBDD/src/Ejercicio2.java
+++ ConexionBBDD/src/Ejercicio2.java
@@ -2,23 +2,86 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Ejercicio2 {
 	
 	public static Scanner entrada = new Scanner(System.in);
 	
-	private static void eliminarJugador() {
+	public static Connection ConexionBD(String url, String usuario, String password) throws SQLException, ClassNotFoundException {
 		
-		String url = "jdbc:mysql://localhost:3306/nba";
-		String usuario = "root";
-		String password = "cfgs";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		return DriverManager.getConnection(url, usuario, password);
+		
+	}
+	
+	private static void ficharJugador(Connection conexion) {
 		
 		try {
 			
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Se ha conectado correctamente a la base de datos\n");
 			
-			Connection conexion = DriverManager.getConnection(url, usuario, password);
+			String consulta = "Select * from equipos";
+			PreparedStatement sentencia = conexion.prepareStatement(consulta);
+			
+			ResultSet resultado = sentencia.executeQuery();
+			
+			System.out.printf("%-15s %-20s %-20s %-10s%n", "Nombre", "Ciudad", "Conferencia", "División");
+			System.out.println("------------------------------------------------------------------------------");
+			
+			while(resultado.next()) {
+				
+				String nombre = resultado.getString("Nombre");
+				String ciudad = resultado.getString("Ciudad");
+				String conferencia = resultado.getString("Conferencia");
+				String division = resultado.getString("Division");
+				
+				System.out.printf("%-15s %-20s %-20s %-10s%n", nombre, ciudad, conferencia, division);
+				
+			}
+			
+			System.out.println("\n");
+			
+			System.out.println("Indique el nombre del equipo para ver sus jugadores y ficharlos: ");
+			String nombre_equipo = entrada.nextLine();
+			
+			System.out.println("\n");
+			
+			String consulta2 = "select * from jugadores where Nombre_equipo = (select Nombre from equipos where Nombre = ?)";
+			PreparedStatement sentencia2 = conexion.prepareStatement(consulta2);
+			
+			sentencia2.setString(1, nombre_equipo);
+			
+			ResultSet resultado2 = sentencia2.executeQuery();
+			
+			while(resultado2.next()) {
+
+				int codigo = resultado2.getInt("codigo");
+				String nombre = resultado2.getString("Nombre");
+				String procedencia = resultado2.getString("Procedencia");
+				String altura = resultado2.getString("Altura");
+				int peso = resultado2.getInt("Peso");
+				String posicion = resultado2.getString("Posicion");
+				String nombreEquipo = resultado2.getString("Nombre_equipo");
+				
+			    System.out.printf("%-15d %-20s %-20s %-10s %-10s %-10s %-10s%n", codigo, nombre, procedencia, altura, peso, posicion, nombreEquipo);
+				
+			}
+			
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+	private static void eliminarJugador(Connection conexion) {
+		
+		try {
 			
 			System.out.println("Se ha conectado correctamente a la base de datos");
 			
@@ -51,9 +114,29 @@ public class Ejercicio2 {
 			entrada.nextLine();
 			
 			String consulta2 = "DELETE from jugadores where codigo = ?";
+			try {
+			
 			PreparedStatement sentencia2 = conexion.prepareStatement(consulta2);
 			
-			ResultSet resultado2 = sentencia.executeQuery();
+			sentencia2.setInt(1, codigo);
+			
+			int resultado2 = sentencia2.executeUpdate();
+			
+			if (resultado2 > 0) {
+	            
+				System.out.println("Jugador eliminado correctamente.");
+	        
+			} else {
+	           
+	        	System.out.println("No se encontró un jugador con ese código.");
+	       
+	        }
+			
+			}catch(Exception e) {
+				
+				e.printStackTrace();
+				
+			}
 		
 		}catch(Exception e) {
 			
@@ -66,6 +149,10 @@ public class Ejercicio2 {
 	public static void main(String[] args) {
 		
 		int opcion = 0;
+		
+		String url = "jdbc:mysql://localhost:3306/nba";
+		String usuario = "root";
+		String password = "cfgs";
 		
 		do {
 		
@@ -85,10 +172,29 @@ public class Ejercicio2 {
 		
 		case 1:
 			
-			eliminarJugador();
+			try(Connection conexion = ConexionBD(url,usuario,password)){
+			
+				eliminarJugador(conexion);
+			
+			}catch(SQLException | ClassNotFoundException e) {
+				
+				e.printStackTrace();
+				
+			}
+			
 			break;
 			
 		case 2:
+			
+			try(Connection conexion = ConexionBD(url,usuario,password)){
+				
+				ficharJugador(conexion);
+				
+				}catch(SQLException | ClassNotFoundException e) {
+					
+					e.printStackTrace();
+					
+				}
 			
 			break;
 			
